ddc91ffab9914096fc67b4bbf5c5301b4f941667
Tim Henderson <tim.tadh@gmail.com>
2009-04-01 15:11:18 -0400

    added default_config to configure twik as well as db.conf an example of how to setup the db configuration file
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

---
+++ default_config.py
@@ -0,0 +1,31 @@
+import sys
+sys.stderr = sys.stdout
+import cgitb; cgitb.enable()
+import warnings
+warnings.simplefilter('ignore', UserWarning)
+
+FIELDS = ['HOST', 'PORT', 'USER', 'PASSWD', 'DB']
+FIELDS.sort()
+
+f = open('db.conf', 'r')
+s = f.read()
+f.close
+
+d = dict([[y.lstrip().rstrip() for y in x.split(':')] for x in s.split('\n') if x and x.lstrip()[0] != '#' and len(x.split(':')) == 2])
+keys = d.keys()
+keys.sort()
+
+try:
+    assert keys == FIELDS
+except AssertionError, e: 
+    print '''You database file does not match the schema:
+        HOST:host
+        PORT:3306
+        USER:user
+        PASSWD:password
+        DB:database'''
+    print 'yours -> \n', s
+
+import db
+for k in keys:
+    db.__setattr__(k, d[k])
